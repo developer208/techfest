9adfb095478ca1e1e4597ae043b062ba13b048bf
developer208 <it.developer2002@gmail.com>
2023-02-09 14:14:33 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer';
+import './Home.css'
+import Navbar from './Navbar';
+import Test from './Test';
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home;
